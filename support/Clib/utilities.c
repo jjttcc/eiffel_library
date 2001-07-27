bf03b27a65fbebca976fe8f0cfa6c33676ac2188
jcochrane <jcochrane@eeff27e6-3d10-0410-b546-810a09abb65b>
2001-07-27 07:45:38 +0000

    Changed return types of functions that are Eiffel "external" C functions to the official types from eif_cecil.h - safer, more portable. Changed try_to_open to return the file descriptor (which needs to be saved by the Eiffel object) of the open file, if it succeeds. Added a close_file routine that takes the fd from try_to_open to close the file.  Added because the Windows version doesn't succeed in unlinking the file (remove_file) unless the file has been closed.  Added a mode argument (-rw------) to the open call in try_to_open to ensure that the file is writable.  Windows needs this in order to delete the file.  Added a couple includes actually needed for open.

--- support/Clib/utilities.c
+++ support/Clib/utilities.c
@@ -9,6 +9,8 @@
 
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <time.h>
 #include <string.h>
 #include <errno.h>
@@ -18,6 +20,7 @@
 #include <io.h>
 #define	NO_NANOSLEEP
 #endif
+#include <eif_cecil.h>
 
 /* Simple and completely innaccurate sleep function implemented with loops */
 void loop_sleep (int seconds, int microseconds) {
@@ -42,8 +45,6 @@ void loop_sleep (int seconds, int microseconds) {
 void microsleep (int seconds, int microseconds) {
 #if defined(NO_NANOSLEEP)
 	loop_sleep (seconds, microseconds);
-#elif defined(POO)
-	bing();
 #else
 	struct timespec timespec;
 	timespec.tv_sec = seconds;
@@ -53,46 +54,52 @@ void microsleep (int seconds, int microseconds) {
 #endif
 }
 
-/* Try to open file `s' if it doesn't exist, returning 0 if successful.  If
- * it exists, if `wait', wait for it to be unlinked; otherwise, return -1.
+/* Try to open file `s' if it doesn't exist, returning the file descriptor
+ * of the resulting open file if successful or -1 if unsuccessful.  If the
+ * file already exists, if `wait', wait for it to be unlinked; otherwise,
+ * return -1.
  * If an error occurs, error_on_last_operation will be true; otherwise, it
  * will be false. If not `wait' and the file exists, open_file_exists will be
  * true; otherwise open_file_exists will be false. */
-int try_to_open (char* s, int wait) {
+EIF_INTEGER try_to_open (char* s, int wait) {
 	int result;
 	int sleep_msecs = 90000;
 
 	errno = 0;
-	result = open (s, O_RDWR | O_CREAT | O_EXCL);
+	result = open (s, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 	if (wait && result == -1) {
 		do {
 			microsleep (0, sleep_msecs);
-			result = open (s, O_RDWR | O_CREAT | O_EXCL);
+			result = open (s, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 		} while (result == -1);
 	}
-	if (result != -1) result = 0;
 	return result;
 }
 
 /* If try_to_open was called with `wait' = true, did it fail because the
  * file exists?  Result is boolean - that is, 0 is false, not 0 is true. */
-int open_file_exists() {
+EIF_BOOLEAN open_file_exists() {
 	return errno == EEXIST;
 }
 
 /* Did an error occur during the last operation?  Result is boolean - that
  * is, 0 is false, not 0 is true. */
-int error_on_last_operation() {
+EIF_BOOLEAN error_on_last_operation() {
 	return errno != 0;
 }
 
+/* Close file with file descriptor `fd'. Return -1 if operation failed. */
+EIF_INTEGER close_file (EIF_INTEGER fd) {
+	return close (fd);
+}
+
 /* Remove file with name `s'. Return -1 if operation failed. */
-int remove_file (char* s) {
+EIF_INTEGER remove_file (char* s) {
 	return unlink (s);
 }
 
 /* Description of last error that occurred. */
-char* last_c_error() {
+EIF_POINTER last_c_error() {
 	return strerror(errno);
 }
 
